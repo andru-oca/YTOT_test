adb2ee34d08955648d3cee0517138c029d2db5f9
andru-oca <112781394+andru-oca@users.noreply.github.com>
2023-12-13 07:51:00 -0300

    Create builder_python_anywhere.sh

---
+++ builder_python_anywhere.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o errexit  # exit on error
+
+pip install -r requirements.txt
+
+mkdir  staticfiles media
+python manage.py collectstatic --no-input
+python manage.py makemigrations
+python manage.py migrate
+
+DJANGO_SUPERUSER_USERNAME=admin \
+DJANGO_SUPERUSER_PASSWORD=123 \
+DJANGO_SUPERUSER_EMAIL="admin@mail.com" \
+python manage.py createsuperuser --noinput
+
